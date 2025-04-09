ec9fa9b2beba1a109e295c3fedb4b4c98307310a
Ubuntu <ubuntu@ip-172-31-8-135.ap-southeast-1.compute.internal>
2025-04-09 09:55:18 +0000

    final_commit

--- includes/connect.php
+++ includes/connect.php
@@ -3,7 +3,7 @@
 // ini_set('display_errors', 1);
 
 // $localhost = "13.127.148.211";
-$localhost = "13.201.70.22";
+$localhost = "13.235.9.65";
 $username = "letsfame";
 $password = "Letsfame@123#123";
 $dbname = "letsfameblog";
