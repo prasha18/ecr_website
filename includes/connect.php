65789f8f8bf5098f48055c6aa8015b567bf14c0b
root <root@ip-172-31-29-104.ap-south-1.compute.internal>
2026-07-23 15:39:58 +0000

    changes connect

--- includes/connect.php
+++ includes/connect.php
@@ -3,7 +3,7 @@
 // ini_set('display_errors', 1);
 
 // $localhost = "13.127.148.211";
-$localhost = "13.235.9.65";
+$localhost = "13.127.148.211";
 $username = "letsfame";
 $password = "Letsfame@123#123";
 $dbname = "letsfameblog";
